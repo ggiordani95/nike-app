56abc07e0bb2261ffbb17313a355188a7b959655
Gustavo Giordani <ggiordani@hotmail.com>
2023-06-01 16:08:00 -0300

    update bottom tab styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,27 @@
 import { Tabs } from 'expo-router'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function Layout() {
   return (
     <Tabs 
-        screenOptions={{
-        headerShown: false,
-      }}
-    />
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, size}) => {
+            return <Icon name={'home'} size={32} color={focused ? 'white' : 'grey'} />;
+          },
+          headerShown: false,
+          tabBarLabel: "Dashboard",
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: '#222222',
+            borderTopWidth: 0,
+          },
+        
+        })}
+        
+    >
+      
+
+    </Tabs>
   )
 }
 
