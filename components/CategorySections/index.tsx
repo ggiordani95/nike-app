2aa498570b413308aa5ebc416b678aaa7231cfe6
Gustavo Giordani <94162641+ggiordani95@users.noreply.github.com>
2023-06-04 19:33:47 -0300

    update category sections

--- components/CategorySections/index.tsx
+++ components/CategorySections/index.tsx
@@ -8,9 +8,12 @@ export default function CategorySections() {
 
   const [categories, setCategories] = useState<Object[] | null>(null);
   const [categoryNames, setCategoryNames] = useState<Array<string>>([]);
-
+  const [sectionFocused, setSectionFocused] = useState<number>(0);
   
   
+  useEffect(()=>{
+ 
+  },[sectionFocused])
 
   useEffect(() => {
     async function fetchingData (){
@@ -18,7 +21,6 @@ export default function CategorySections() {
       setCategories(fullCategories);
       const newCategoryNames = fullCategories.map((category: any) => category.category_name);
       setCategoryNames([...categoryNames, ...newCategoryNames]);
-      console.log(categoryNames);
       
     }
     fetchingData();
@@ -31,27 +33,33 @@ export default function CategorySections() {
       
     }
   }, [])
-  
-  const SectionTextComponent = ({item, index}: any) => {
-    
-    const [isFocusIndex, setIsFocusIndex] = useState<number>(0);
-    
+
+
+  function handleIndex(indexSection: any){
+    setSectionFocused(indexSection);
+  }
+
+
+  const SectionTextComponent = ({item, index, focused, indexSectionFocused}: any) => {
+    function handleSectionFocused(index: number){
+      indexSectionFocused(index);
+    }
     return (
       <>
-        <TouchableOpacity onPress={()=> setIsFocusIndex(index)}>
-          <SectionText style={{fontWeight: isFocusIndex === index ? 'bold' : '400'}}>{item}</SectionText>
+        <TouchableOpacity onPress={()=> handleSectionFocused(index)}>
+          <SectionText style={{fontWeight: focused ? 'bold' : '400', textDecorationLine: focused ? 'underline' : 'none'}}>{item}</SectionText>
         </TouchableOpacity>
-      
       </>
     )
   }
+
 
   return (
     <CategorySectionsContainer>
       <FlatList
           data={categoryNames}
           keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item, index }: any) => <SectionTextComponent item={item} index={index}/>}
+          renderItem={({ item, index }: any) => <SectionTextComponent item={item} index={index} indexSectionFocused={handleIndex} focused={sectionFocused == index ? true : false}/>}
           horizontal={true}
           showsHorizontalScrollIndicator={false}
          
