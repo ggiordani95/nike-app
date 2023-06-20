2924589cc3a5b03d51053321f92a3c5e586a3642
Gustavo Giordani <94162641+ggiordani95@users.noreply.github.com>
2023-06-20 00:20:18 -0300

    update

--- app/(tabs)/adorables/index.tsx
+++ app/(tabs)/adorables/index.tsx
@@ -23,8 +23,8 @@ export default function index() {
         <Image source={logo} style={{height: 45, width: 45, top: 10}}/>
       </View>
       <FlatList data={atFavorites} renderItem={({item,index}:any)=>(
-        <Link href="/" style={{flexDirection:'row',height:height / 8, width: width * 0.9, justifyContent:'flex-start',alignItems:'center', backgroundColor:'#f3f3f3',marginVertical:12}} key={index}>
-          <Image source={{ uri: item.image }} style={{height:height / 8, width:width * 0.2, borderWidth:1, borderColor:'#e7e7e7', borderRadius: 14}}/>
+        <Link href="/" style={{flexDirection:'row',height:height / 6, width: width * 0.9, justifyContent:'flex-start',alignItems:'center', backgroundColor:'#f3f3f3',marginVertical:12}} key={index}>
+          <Image source={{ uri: item.image }} style={{height:height / 9, width:width * 0.2, borderWidth:1, borderColor:'#e7e7e7', borderRadius: 14}}/>
           <View style={{flexDirection:'column', alignItems: 'flex-start',justifyContent:'center', gap: 4,paddingLeft:14,width: width * 0.6, height:'100%'}}>
             <Text style={{fontSize:RFValue(12),fontWeight:'500'}}>{item.name}</Text>
            
